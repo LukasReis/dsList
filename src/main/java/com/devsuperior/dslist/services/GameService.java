b0f254ba2d1276a361defab329f4ae6239213699
LukasReis <luke.silvareis@gmail.com>
2023-07-13 13:05:07 -0300

    Deploy ready

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.services;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepsitory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,7 +28,11 @@ public class GameService {
         return resultado.stream().map(x -> new GameMinDTO(x)).toList();
     }
 
-
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> resultado = gameRepsitory.searchByList(listId);
+        return resultado.stream().map(x -> new GameMinDTO(x)).toList();
+    }
 
 
 }
